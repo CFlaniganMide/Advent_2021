5f4b83e21388e243be94512d3808a0e828e9e39f
MIDE\cflanigan <cflanigan@mide.com>
2021-12-09 10:27:06 -0500

    add day1 unit tests

--- src/day1/day1.rs
+++ src/day1/day1.rs
@@ -2,34 +2,66 @@ use std::path;
 
 fn run_day1(filename: &str) {
 
-    day1_part1(filename);
+    day1_part1(input::input_as_iter::<i64>(path::Path::new(filename)));
 
-    day1_part2(filename);
+    day1_part2(input::input_as_iter::<i64>(path::Path::new(filename)));
 
 }
 
-fn day1_part1(filename: &str) {
+fn day1_part1(in_vals: Vec<i64>) -> u64{
 
-    let in_vals = input::input_as_iter::<i64>(path::Path::new(filename));
-
-    let pos_diff = array_math::apply(array_math::diff::<i64>(in_vals), |x| x > 0);
+    let pos_diff = array_math::apply(
+        array_math::diff::<i64>(in_vals),
+        |x| x > 0,
+    );
 
     let n_pos = array_math::count_true(pos_diff);
 
     println!("Part 1: {} diffs greater than 0", n_pos);
 
+    return n_pos;
+
 }
 
-fn day1_part2(filename: &str) {
+fn day1_part2(in_vals: Vec<i64>) -> u64{
 
-    let in_vals = input::input_as_iter::<i64>(path::Path::new(filename));
+    let convolved = array_math::convolve(
+        in_vals,
+        vec![1, 1, 1],
+        "same",
+    );
 
-    let convolved = array_math::convolve(in_vals, vec![1, 1, 1], "same");
-
-    let pos_diff = array_math::apply(array_math::diff::<i64>(convolved), |x| x > 0);
+    let pos_diff = array_math::apply(
+        array_math::diff::<i64>(convolved),
+        |x| x > 0,
+    );
 
     let n_pos = array_math::count_true(pos_diff);
 
     println!("Part 2: {} diffs greater than 0", n_pos);
 
+    return n_pos;
+
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn test_day1_part1() {
+        let test_input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        let target = 7;
+
+        assert_eq!(day1_part1(test_input), target);
+    }
+
+    #[test]
+    fn test_day1_part2() {
+        let test_input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        let target = 5;
+
+        assert_eq!(day1_part2(test_input), target);
+    }
 }
